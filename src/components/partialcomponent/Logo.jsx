42a979e962af9343262d88fb7586947b37f36773
shrey7ansh07 <shreyguptaji007@gmail.com>
2023-12-21 20:33:47 +0530

    added the alt field

--- src/components/partialcomponent/Logo.jsx
+++ src/components/partialcomponent/Logo.jsx
@@ -3,7 +3,7 @@ import edit from "../images/edit.png"
 function Logo() {
   return (
     <div className='flex items-center md:gap-2 gap-1'>
-        <img src={edit} alt="" className='md:w-10 w-4' />
+        <img src={edit} alt="logo" className='md:w-10 w-4' />
         <div className='md:text-3xl text-[1rem] font-semibold gradient-install'>BLOGGER</div>
     </div>
   )
