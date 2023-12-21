66000f81ede2cf81029086a8666fe87954fe1445
shrey7ansh07 <shreyguptaji007@gmail.com>
2023-12-21 20:31:52 +0530

    redirecting to login after signup and handling err

--- src/components/Login/SigninFeilds.jsx
+++ src/components/Login/SigninFeilds.jsx
@@ -1,18 +1,38 @@
-import {Input, signupFields,FormAction,ErrorDisplay} from '../index'
+import {Input,FormAction,ErrorDisplay} from '../index'
 import { useState } from 'react';
 import {useForm} from "react-hook-form"
+import { register as registeUser } from '../../services/authServices';
+import { useNavigate } from 'react-router-dom';
 
 function SigninFeilds() {
-  const {register, handleSubmit,watch, formState: {errors}} = useForm()
+  const navigate = useNavigate();
+  const {register, handleSubmit,watch, formState: {errors},reset} = useForm()
   const password = watch("password")
   const [error, seterror] = useState("")
   const signin = async (data) => {
-    console.log(data)
+    const user = 
+    {
+      username: data.username,
+      fullname: data.fullname,
+      email: data.email,
+      password: data.password
+    }
+    try
+    {
+      const response = await registeUser(user)
+      seterror("")
+      reset();
+      navigate("/Login")
+    }
+    catch(e)
+    {
+      seterror(e.message || "something went wrong while registering")
+    }
   }
     return(
     <form action="" onSubmit={handleSubmit(signin)}>
     
-      {/* {(Object.keys(errors).length > 0 && seterror(errors.Object.keys(errors)[0].message)&& <ErrorDisplay message = {error}></ErrorDisplay>)||seterror("")} */}
+      {error !=="" && <ErrorDisplay message = {error}></ErrorDisplay>}
       <Input 
       labelFor = "fullname"
       labelText = "Fullname"
