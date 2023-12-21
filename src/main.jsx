2d33ee7bfc3f2d26f692ed3206bee20739c4226e
shrey7ansh07 <shreyguptaji007@gmail.com>
2023-12-21 20:27:05 +0530

    modified main.jsx for app.jsx and used redux store

--- src/main.jsx
+++ src/main.jsx
@@ -1,41 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import { Layout,Home,About,Report,Login,Signin } from "./components/index"
+import { RouterProvider} from 'react-router-dom'
+import { router } from "./components/index"
+import { Provider } from 'react-redux'
+import {store} from "./store/store.js"
+import App from './app'
 
-const router = createBrowserRouter([
-  {
 
-    path: "/",
-    element: <Layout/>,
-    children: [
-      {
-        path: "",
-        element: <Home/>
-      },
-      {
-        path: "about",
-        element: <About/>
-      },
-      {
-        path: "report",
-        element: <Report/>
-      },
-      {
-        path: "Login",
-        element: <Login/>
-      },
-      {
-        path: "Signin",
-        element: <Signin/>
-      }
-    ]
-  }
-])
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-  <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+  <React.StrictMode>    
+    <Provider store={store}>
+      <App/>
+    </Provider>
   </React.StrictMode>,
 )
