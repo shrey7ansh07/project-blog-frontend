26d76d85807d05ad0a4a3e9e5a40cbcc45a41427
shrey7ansh07 <shreyguptaji007@gmail.com>
2023-12-21 20:39:58 +0530

    removed router provider and routes file

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { RouterProvider} from 'react-router-dom'
-import { router } from "./components/index"
 import { Provider } from 'react-redux'
 import {store} from "./store/store.js"
 import App from './app'
