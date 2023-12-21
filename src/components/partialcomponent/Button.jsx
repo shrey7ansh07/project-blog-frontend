32882d56ad2da48d20f9ac1c25a7d8a98fb9a47d
shrey7ansh07 <shreyguptaji007@gmail.com>
2023-12-21 20:32:57 +0530

    removing onsubmit handler

--- src/components/partialcomponent/Button.jsx
+++ src/components/partialcomponent/Button.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 function Button(props) {
   return (
     <button className='md:text-xs text-[8px] border-[1.5px] border-gray-400 md:px-6 px-2 rounded-lg text-gray-100 md:h-[30px] h-auto
-    gradient-install-hover active:scale-110 transition-all duration-500 ease-out sm:py-[4px]'>
+    gradient-install-hover active:scale-110 transition-all duration-500 ease-out sm:py-[4px]' > 
         <NavLink to={props.reach}>{props.name}</NavLink></button>
     )
 }
