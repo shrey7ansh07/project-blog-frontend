c16adeb6ef859778bfae3213bbe2cf07aed7c0c1
shrey7ansh07 <shreyguptaji007@gmail.com>
2023-12-26 17:26:53 +0530

    controlled components loaded

--- src/components/RoutingComp/RoutingComp.jsx
+++ src/components/RoutingComp/RoutingComp.jsx
@@ -1,14 +1,31 @@
 import React from 'react'
 import { RoutingElem } from '../index';
+import { useSelector } from 'react-redux';
 
 function RoutingComp() {
-  return (
-    <nav className='flex md:gap-5 gap-3 items-center mx-5'>
-        <RoutingElem name="Home" reach=""/>
-        <RoutingElem name="About" reach = "/about"/>
-        <RoutingElem name="Report" reach = "/report"/>
-    </nav>
-  )
+  const isAuthenticated = useSelector(state => state.authAnduser.isAuthenticated)
+  if (!isAuthenticated) 
+  {
+    return(      
+        <nav className='flex md:gap-7 gap-4 items-center justify-start md:mx-[80px] mx-[50px]  flex-1'>
+            <RoutingElem name="Home" reach=""/>
+            <RoutingElem name="About" reach = "/about"/>
+            <RoutingElem name="Report" reach = "/report"/>
+        </nav>
+        )
+  }
+  else
+  {
+    return (
+      <nav className='flex md:gap-7 gap-4 items-center justify-start md:mx-[80px] mx-[50px]  flex-1'>
+            <RoutingElem name= "Dashboard" reach="/dashboard"/>
+            <RoutingElem name= "Profile" reach= "/edit_profile"/>
+            <RoutingElem name= "Add Blog" reach= "/writeBlog"/>
+            
+      </nav>
+    )
+  }
+
 }
 
 export default RoutingComp
