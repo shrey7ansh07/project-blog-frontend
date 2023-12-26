cc7df81432dec54c53887c50f6e6dd2f606791db
shrey7ansh07 <shreyguptaji007@gmail.com>
2023-12-26 17:26:02 +0530

    removing redux persist from the store

--- src/store/store.js
+++ src/store/store.js
@@ -5,3 +5,33 @@ export const store = configureStore({
         authAnduser: authAnduserReducer
     }
 })
+
+// import { configureStore } from '@reduxjs/toolkit';
+// import { persistReducer, persistStore,
+//     FLUSH,
+//     REHYDRATE,
+//     PAUSE,
+//     PERSIST,
+//     PURGE,
+//     REGISTER, } from 'redux-persist';
+
+// import storage from 'redux-persist/lib/storage';
+// import authAnduserReducer from './authSlice.js';
+
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+// };
+
+// const persistedAuthReducer = persistReducer(persistConfig, authAnduserReducer);
+
+// export const store = configureStore({
+//   reducer: {
+//     authAnduser: persistedAuthReducer,
+//     // middleware: getDefaultMiddleware =>
+//     // getDefaultMiddleware({
+//     //   serializableCheck: false,
+//     // })
+// }});
+
+// export const persistor = persistStore(store);
