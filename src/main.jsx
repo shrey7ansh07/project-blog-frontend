adff924343e34d30071803beba3615a0c98bc42f
shrey7ansh07 <shreyguptaji007@gmail.com>
2023-12-26 17:33:24 +0530

    changes in store

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { Provider } from 'react-redux'
-import {store} from "./store/store.js"
+import { store} from './store/store.js'; // adjust the path as needed
+// import { PersistGate } from 'redux-persist/integration/react';
 import App from './app'
 
 
@@ -10,7 +11,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
   <React.StrictMode>    
     <Provider store={store}>
-      <App/>
+      {/* <PersistGate persistor={persistor}> */}
+        <App/>
+      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>,
 )
