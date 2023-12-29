8b7cb713f3dedaee2055b70ff279fa647aef8fc4
shrey7ansh07 <shreyguptaji007@gmail.com>
2023-12-30 01:24:15 +0530

    blog services to handle blog api calls

---
+++ src/services/blogServices.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+
+
+const postBlog = async(data) => {
+    try {
+        const response = await axios.post("/api/v1/blogs/addblog",data)
+        console.log(response)
+    } catch (error) {
+        throw error.response.data
+    }
+}
+
+const getBlogs = async(userId) => {
+    try {
+        const response = await axios.get(`/api/v1/blogs/getblogs/${userId}`)
+        return response.data.data.blogs
+    }
+    catch
+    {
+        return error.response.data
+    }
+}
+
+const editBlogs = async(userId,data) => {
+    try {
+        const response = await axios.patch(`/api/v1/blogs/editblog/${userId}`,data)
+        return response.data.data.blogs
+    }
+    catch
+    {
+        return error.response.data
+    }
+}
+
+const deleteBlog = async(blogId) => {
+try {
+        const response = await axios.post(`/api/v1/blogs/deleteblog/${blogId}`)
+        return response.data.data.blogs
+} catch (error) {
+    throw error.response.data
+}
+}
+export {
+    postBlog,
+    getBlogs,
+    editBlogs,
+    deleteBlog
+}
