e217dd3f12dfeddafd276699df853cbb25bff96e
shrey7ansh07 <shreyguptaji007@gmail.com>
2023-12-30 01:34:21 +0530

    introduced more components and dispatched on load

--- src/app.jsx
+++ src/app.jsx
@@ -1,42 +1,50 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Layout, Home, About, Report, Login, Signin } from './components/index.js';
+import { Layout, Home, About, Report, Login, Signin, DisplayBlog } from './components/index.js';
 import Dashboard from './components/Dashboard/Dashboard.jsx';
-import EditProfile from './components/Dashboard/EditProfile.jsx';
-import Editor from './components/Dashboard/Editor.jsx';
-import { useDispatch } from 'react-redux';
+import EditProfile from './components/EditProfile/EditProfile.jsx';
+import Editor from './components/Editor/Editor.jsx';
+import { useDispatch,useSelector } from 'react-redux';
 import { setAuthenticated } from './store/authSlice.js';
 import { isLoggedIn } from './services/authServices.js';
+import { getBlogs } from './services/blogServices.js';
+import { loadBlogs } from './store/blogSlice.js';
 
 
 function App() {
   const dispatch = useDispatch()
+
   useEffect(() => {
-    const checkAuth = async () => {
-      try {
-        const response = await isLoggedIn()
-        const newUser = {
-          fullname : response.fullname,
-          userId : response._id,
-          email : response.email,
-          username : response.username,
-          quote : response.quote,
-          bio : response.bio,
-          coverimage : response.coverimage,
-          links : response.links,
-          followers : response.followers,
-          following : response.following,
-          //* here we will have one 
-      }
-        dispatch(setAuthenticated({user:newUser}))
-      } catch (error) {
-        console.error('Failed to check auth', error);
-      }
+    const checkAuth = async () => {   
+          try {
+          const response = await isLoggedIn()
+          const newUser = {
+            fullname : response.fullname,
+            userId : response._id,
+            email : response.email,
+            username : response.username,
+            quote : response.quote,
+            bio : response.bio,
+            coverimage : response.coverimage,
+            links : response.links,
+            followers : response.followers,
+            following : response.following,
+            //* here we will have one 
+        }
+          dispatch(setAuthenticated({user:newUser}))
+          const userBlogs = await getBlogs(newUser.userId)
+          dispatch(loadBlogs({blogs:userBlogs}))
+    
+        } catch (error) {
+          console.log("error while logging in");
+        }
+    
     };
 
     checkAuth();
   }, []);
+
   return (
     <Router>
       <Routes>
@@ -49,6 +57,7 @@ function App() {
           <Route path="dashboard" element={<Dashboard/>}/>
           <Route path="edit_profile" element={<EditProfile/>}/>
           <Route path="writeBlog" element={<Editor/>}/>
+          <Route path="readblog" element={<DisplayBlog/>}/>
         </Route>
       </Routes>
     </Router>
