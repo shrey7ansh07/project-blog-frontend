6cc800218c2881c73ef5a7be1c9b7f1307b6379d
shrey7ansh07 <shreyguptaji007@gmail.com>
2023-12-30 01:33:02 +0530

    introduced blog container

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import Admin from './ProfileComp/Admin'
-import {Heading} from "../index"
+import {Heading,BlogContainer} from "../index"
+import { useSelector } from 'react-redux'
 function Dashboard() {
+  const user = useSelector(state => state.authAnduser.userData)
+  const blogs = useSelector(state=> state.myblogs.defaultBlogsUser)
+
   return (
     <div>
         <Admin/>
         <Heading text = "My Blogs"/>
+        <BlogContainer 
+        blogs={blogs}
+        userimage={user?.coverimage}
+        username={user?.fullname}
+        />
+        <Heading text = "Continue Reading"/>
+        
 
     </div>
   )
