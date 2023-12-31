6a8e54655c0a1fa82e92a35abd1fa1d7c9af7dda
shrey7ansh07 <shreyguptaji007@gmail.com>
2023-12-31 23:32:40 +0530

    components shuffling

--- src/components/index.js
+++ src/components/index.js
@@ -1,28 +1,28 @@
 import Header from "./Header/Header";
-import Footer from "./footer/Footer";
+import Footer from "./Footer/Footer";
 import Logo from "./partialcomponent/Logo";
 import Button from "./partialcomponent/Button";
-import RoutingComp from "./RoutingComp/RoutingComp";
-import RoutingElem from "./RoutingComp/RoutingElem";
-import RoutingElem2 from "./RoutingComp/RoutingElem2";
-import RoutingComp2 from "./RoutingComp/RoutingComp2";
-import Layout from "./outlet/Layout";
-import Home from "./MainComponents/Home";
-import About from "./MainComponents/About";
-import Report from "./MainComponents/Report";
-import Login from "./Login/Login";
-import Signin from "./Login/Signin";
+import RoutingComp from "./RoutingComp/RoutingCompHeader/RoutingComp";
+import RoutingElem from "./RoutingComp/RoutingCompHeader/RoutingElem";
+import RoutingElem2 from "./RoutingComp/RoutingCompFooter/RoutingElem2";
+import RoutingComp2 from "./RoutingComp/RoutingCompFooter/RoutingComp2";
+import Layout from "./Outlet/Layout";
+import Home from "./MainComponents/HomeComponents/Home";
+import About from "./MainComponents/AboutComponents/About";
+import Report from "./MainComponents/ReportComponents/Report";
+import Login from "./Login/LoginComponents/Login";
+import Signin from "./Login/SignUpComponents/Signin";
 import SocialMediaLinks from "./SocialMediaLinks/SocialMediaLinks";
-import HomeStart from "./partialcomponent/HomeStart";
-import HomeMid from "./partialcomponent/HomeMid";
+import HomeStart from "./MainComponents/HomeComponents/HomeStart";
+import HomeMid from "./MainComponents/HomeComponents/HomeMid";
 import Heading from "./partialcomponent/Heading";
-import Intro from "./partialcomponent/Intro";
-import Input from "./Login/Input";
-import LoginFeilds from "./Login/LoginFeilds";
-import FormExtra from "./Login/FormExtra";
-import FormAction from "./Login/FormAction";
-import SigninFeilds from "./Login/SigninFeilds";
-import AboutMid from "./partialcomponent/AboutMid";
+import Intro from "./MainComponents/HomeComponents/Intro";
+import Input from "./Login/ExtraCompontents/Input";
+import LoginFeilds from "./Login/LoginComponents/LoginFeilds";
+import FormExtra from "./Login/ExtraCompontents/FormExtra";
+import FormAction from "./Login/ExtraCompontents/FormAction";
+import SigninFeilds from "./Login/SignUpComponents/SigninFeilds";
+import AboutMid from "./MainComponents/AboutComponents/AboutMid";
 import ErrorDisplay from "./partialcomponent/ErrorDisplay";
 import authAnduserReducer from "../store/authSlice.js"
 import UserImage from "./Dashboard/ProfileComp/SubProfileComp/UserImage";
@@ -32,12 +32,14 @@ import BlogCardUser from "./DisplayBlog/Myblogs/BlogCardUser";
 import BlogContainer from "./DisplayBlog/Myblogs/BlogContainer";
 import DisplayBlog from "./DisplayBlog/CardComponents/DisplayBlog";
 import CustomBtn from "./EditProfile/EditProfSub/CustomBtn";
-import { deleteBlog,updatestatus,getPopular } from "../services/blogServices";
+import { deleteBlog, updatestatus, getPopular } from "../services/blogServices";
 import DefaultMsg from "./Dashboard/ProfileComp/SubProfileComp/DefaultMsg";
 import BlogContainerPopular from "./DisplayBlog/MostPopular/BlogContainerPopular";
+import Privacy from "./MainComponents/PrivacyComponents/Privacy";
 
 
 export {
+    Privacy,
     BlogContainerPopular,
     getPopular,
     DefaultMsg,
@@ -77,7 +79,7 @@ export {
     authAnduserReducer,
     UserImage,
     UserName
-    
-  
+
+
 
 }
