362caf77dbf925ded2823bc6ec9603442d5a13ae
shrey7ansh07 <shreyguptaji007@gmail.com>
2023-12-31 17:08:07 +0530

    shifted the report route over authenticated one

--- src/components/RoutingComp/RoutingComp.jsx
+++ src/components/RoutingComp/RoutingComp.jsx
@@ -4,24 +4,23 @@ import { useSelector } from 'react-redux';
 
 function RoutingComp() {
   const isAuthenticated = useSelector(state => state.authAnduser.isAuthenticated)
-  if (!isAuthenticated) 
-  {
-    return(      
-        <nav className='flex md:gap-7 gap-4 items-center justify-start md:mx-[80px] mx-[50px]  flex-1'>
-            <RoutingElem name="Home" reach=""/>
-            <RoutingElem name="About" reach = "/about"/>
-            <RoutingElem name="Report" reach = "/report"/>
-        </nav>
-        )
-  }
-  else
-  {
+  if (!isAuthenticated) {
     return (
       <nav className='flex md:gap-7 gap-4 items-center justify-start md:mx-[80px] mx-[50px]  flex-1'>
-            <RoutingElem name= "Dashboard" reach="/dashboard"/>
-            <RoutingElem name= "Profile" reach= "/edit_profile"/>
-            <RoutingElem name= "Add Blog" reach= "/writeBlog"/>
-            
+        <RoutingElem name="Home" reach="" />
+        <RoutingElem name="About" reach="/about" />
+      </nav>
+    )
+  }
+  else {
+    return (
+      <nav className='flex md:gap-7 gap-4 items-center justify-start md:mx-[80px] mx-[50px]  flex-1'>
+        <RoutingElem name="Dashboard" reach="/dashboard" />
+        <RoutingElem name="Profile" reach="/edit_profile" />
+        <RoutingElem name="Add Blog" reach="/writeBlog" />
+        <RoutingElem name="Report" reach="/report" />
+
+
       </nav>
     )
   }
