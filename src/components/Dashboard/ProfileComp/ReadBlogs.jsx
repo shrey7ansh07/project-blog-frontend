15e432b625509aeb5867a87053229e9e80a497a7
shrey7ansh07 <shreyguptaji007@gmail.com>
2023-12-31 23:27:00 +0530

    shifted into their corresponding folders

--- src/components/Dashboard/ProfileComp/ReadBlogs.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ReadBlogs() {
-  return (
-    <div>ReadBlogs</div>
-  )
-}
-
-export default ReadBlogs
--- src/components/partialcomponent/Community.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Community(props) {
-  return (
-    <div>{props.review}</div>
-  )
-}
-
-export default Community
