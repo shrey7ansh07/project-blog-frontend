fd20d561b069ccdd64fb086d552b213190fddee6
Shreyansh Gupta <120119737+shrey7ansh07@users.noreply.github.com>
2024-07-04 19:09:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api/v1': 'https://project-blog-backend.vercel.app'
+//   },
+  
+// }})
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api/v1': 'https://project-blog-backend.vercel.app'
-  },
-  
-}})
+      '/api/v1': {
+        target: 'https://project-blog-backend.vercel.app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/v1/, '')
+      }
+    }
+  }
+});
+
