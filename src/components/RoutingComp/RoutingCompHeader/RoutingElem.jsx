95d7131412fb36e2e75900b7e314e35bdbc188c9
Shreyansh Gupta <shreyguptaji007@gmail.com>
2024-08-11 00:41:28 +0530

    text size changes

--- src/components/RoutingComp/RoutingCompHeader/RoutingElem.jsx
+++ src/components/RoutingComp/RoutingCompHeader/RoutingElem.jsx
@@ -3,8 +3,8 @@ import {NavLink} from "react-router-dom"
 
 function RoutingElem(props) {
   return (
-    <NavLink to={props.reach} className={({isActive}) => `${!isActive? 
-      "text-gray-100":"gradient-install border-b-[1px] border-orange-400 outline-offset-4"} md:text-[14px] text-[10px] transition-all duration-500 ease-out font-semibold hover:text-[#ffc900]`}>{props.name}</NavLink>
+    <NavLink to={props.reach} className={({isActive}) => `${!isActive?
+      "text-gray-100":"gradient-install border-b-[1px] border-orange-400 outline-offset-4"} laptop:text-[18px] tablet:text-[13px] text-[11px] transition-all duration-500 ease-out font-semibold hover:text-[#ffc900]`}>{props.name}</NavLink>
 
   )
 }
--- src/components/partialcomponent/Logo.jsx
+++ src/components/partialcomponent/Logo.jsx
@@ -4,7 +4,7 @@ function Logo() {
   return (
     <div className='flex items-center md:gap-2 gap-1'>
       <img src={edit} alt="logo" className='md:w-10 w-4' />
-      <div className='md:text-3xl text-[1rem] font-semibold gradient-install'>BLOGEZZY</div>
+      <div className='laptop:text-3xl tablet:text-2xl text-[1rem] font-semibold gradient-install'>BLOGEZZY</div>
     </div>
   )
 }
